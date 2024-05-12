05442a7a4b2b429904bfbd056c4d9a93b59d38ae
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-05-13 02:38:51 +0300

    Update the styles in the login form

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -12,7 +12,7 @@ function LoginPage() {
                     <input type="email"  className='block w-72 py-2.3 px-0 text-sm: text-gray-900 bg-transparent border-o border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer required:' placeholder="Email address " />
                     <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 '></label>
                 </div>
-                <div  className='relative my-4'>
+                <div >
                     <input type="password" className='block-w-72 py-2.3 px-0 text-sm: text-white bg-transparent border-o border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' placeholder="Password"  />
                     <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'></label>
                 </div>
