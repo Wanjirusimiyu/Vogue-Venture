2d45247f95d65904cb26a98ff08a6de4c1f5adaa
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-05-13 03:25:47 +0300

    Edt the NewsLetters title

--- src/pages/home/NewsLetters.jsx
+++ src/pages/home/NewsLetters.jsx
@@ -5,7 +5,7 @@ function NewsLetters() {
     <div className='bg-[#1E2832] bg-opacity-5 xl:px-28 px-4 py-18 mt-10'>
         {/* NewsLetters */}
       <div>
-         <h2 className='title mb-8 mt-20'></h2>
+         <h2 className='title mb-8 mt-20'>Subscribe to our NewsLetters</h2>
          <form className='md:w-1/2 mx-auto text-center'>
             <input type='email' name='email' id='email' placeholder='Email Address....' 
             className='h-8 bg-transparent outline-none border-b-2 pl-2 border-black md-w-2/3 w-full mb-5
