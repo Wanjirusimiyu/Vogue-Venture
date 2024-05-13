57eeeb9b37d9bc4b91c939f3c6bff9c085517dfb
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-05-13 03:10:24 +0300

    Edit the styles in the Banner component

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -14,7 +14,7 @@ function Banner() {
               <h1 className='text-5xl font-bold mb-5'>Collections</h1>
               <p className='text-xl mb-7'>Shop many different collections from various brands</p>
 
-              <button className="bg-black hover:bg-orange-500 px-6 py-2 text-white font-semibold rounded sm flex:items-center gap-2" > <FaShoppingBag className='inline-flex' />Shop Now</button>
+              <button className="bg-black hover:bg-violet-600 px-6 py-2 text-white font-semibold rounded sm flex:items-center gap-2" > <FaShoppingBag className='inline-flex' />Shop Now</button>
             </div>
             <div >
             </div>
